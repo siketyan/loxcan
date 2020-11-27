acc1e36a60416af3718df577950f09c4bedf2e1e
Naoki Ikeguchi <root@siketyan.dev>
2020-11-27 21:09:36 +0900

    ✨ Support Semantic Versioning

---
+++ tests/Versioning/SemVer/SemVerVersionComparatorTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Loxcan\Versioning\SemVer;
+
+use PHPUnit\Framework\TestCase;
+use Siketyan\Loxcan\Versioning\VersionDiff;
+
+class SemVerVersionComparatorTest extends TestCase
+{
+    private SemVerVersionComparator $comparator;
+
+    protected function setUp(): void
+    {
+        $this->comparator = new SemVerVersionComparator();
+    }
+
+    public function test(): void
+    {
+        $this->assertCompare(
+            VersionDiff::UPGRADED,
+            new SemVerVersion(1, 2, 3),
+            new SemVerVersion(2, 0, 0),
+        );
+
+        $this->assertCompare(
+            VersionDiff::DOWNGRADED,
+            new SemVerVersion(1, 2, 3, ['beta', 1]),
+            new SemVerVersion(1, 2, 3, ['beta']),
+        );
+
+        $this->assertCompare(
+            VersionDiff::UPGRADED,
+            new SemVerVersion(1, 2, 3, ['beta']),
+            new SemVerVersion(1, 2, 3, ['rc']),
+        );
+
+        $this->assertCompare(
+            VersionDiff::DOWNGRADED,
+            new SemVerVersion(1, 2, 3),
+            new SemVerVersion(1, 2, 3, ['alpha']),
+        );
+
+        $this->assertCompare(
+            VersionDiff::UPGRADED,
+            new SemVerVersion(1, 2, 3, ['alpha']),
+            new SemVerVersion(1, 2, 3, ['beta']),
+        );
+
+        $this->assertCompare(
+            VersionDiff::UPGRADED,
+            new SemVerVersion(1, 2, 3, ['alpha', 12]),
+            new SemVerVersion(1, 2, 3, ['alpha', 34]),
+        );
+
+        $this->assertCompare(
+            null,
+            new SemVerVersion(1, 2, 3, [], [123]),
+            new SemVerVersion(1, 2, 3, [], [456]),
+        );
+
+        $this->assertCompare(
+            null,
+            new SemVerVersion(2, 3, 4),
+            new SemVerVersion(2, 3, 4),
+        );
+    }
+
+    private function assertCompare(?int $type, SemVerVersion $before, SemVerVersion $after): void
+    {
+        $diff = $this->comparator->compare($before, $after);
+
+        if ($type === null) {
+            $this->assertNull($diff);
+
+            return;
+        }
+
+        $this->assertSame($type, $diff->getType());
+        $this->assertSame($before, $diff->getBefore());
+        $this->assertSame($after, $diff->getAfter());
+    }
+}
---
+++ tests/Versioning/SemVer/SemVerVersionParserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Loxcan\Versioning\SemVer;
+
+use PHPUnit\Framework\TestCase;
+use Siketyan\Loxcan\Exception\UnsupportedVersionException;
+
+class SemVerVersionParserTest extends TestCase
+{
+    private SemVerVersionParser $parser;
+
+    protected function setUp(): void
+    {
+        $this->parser = new SemVerVersionParser();
+    }
+
+    public function test(): void
+    {
+        $version = $this->parser->parse('1.2.3-dev.1.23+build.456.alpha');
+        $this->assertSame(1, $version->getMajor());
+        $this->assertSame(2, $version->getMinor());
+        $this->assertSame(3, $version->getPatch());
+        $this->assertSame(['dev', 1, 23], $version->getPreRelease());
+        $this->assertSame(['build', 456, 'alpha'], $version->getBuild());
+
+        $version = $this->parser->parse('3.4.5-dev');
+        $this->assertSame(3, $version->getMajor());
+        $this->assertSame(4, $version->getMinor());
+        $this->assertSame(5, $version->getPatch());
+        $this->assertSame(['dev'], $version->getPreRelease());
+        $this->assertSame([], $version->getBuild());
+
+        $version = $this->parser->parse('2.3.4+abc12345');
+        $this->assertSame(2, $version->getMajor());
+        $this->assertSame(3, $version->getMinor());
+        $this->assertSame(4, $version->getPatch());
+        $this->assertSame([], $version->getPreRelease());
+        $this->assertSame(['abc12345'], $version->getBuild());
+
+        $version = $this->parser->parse('4.3.2');
+        $this->assertSame(4, $version->getMajor());
+        $this->assertSame(3, $version->getMinor());
+        $this->assertSame(2, $version->getPatch());
+        $this->assertSame([], $version->getPreRelease());
+        $this->assertSame([], $version->getBuild());
+    }
+
+    public function testUnsupported(): void
+    {
+        $this->expectException(UnsupportedVersionException::class);
+
+        $this->parser->parse('v1.2.3_not_supported');
+    }
+}
