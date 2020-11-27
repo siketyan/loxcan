2a26a627b1f70d6dd909db9418ab9291665f0086
Naoki Ikeguchi <root@siketyan.dev>
2020-11-27 22:07:31 +0900

    ✨ Pub (pubspec.lock) scanner support

---
+++ src/Scanner/Pub/PubPackagePool.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Loxcan\Scanner\Pub;
+
+use Siketyan\Loxcan\Scanner\AbstractPackagePool;
+
+class PubPackagePool extends AbstractPackagePool
+{
+}
