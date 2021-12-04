dd63fa8b5f9c96d8be275e46e64ef6941982258b
Naoki Ikeguchi <me@s6n.jp>
2021-12-05 01:56:00 +0900

    ✨ pnpm support

---
+++ src/Scanner/Pnpm/PnpmScanner.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\Loxcan\Scanner\Pnpm;
+
+use Eloquent\Pathogen\PathInterface;
+use Siketyan\Loxcan\Model\DependencyCollectionPair;
+use Siketyan\Loxcan\Model\FileDiff;
+use Siketyan\Loxcan\Scanner\ScannerInterface;
+
+class PnpmScanner implements ScannerInterface
+{
+    private PnpmLockParser $parser;
+
+    public function __construct(
+        PnpmLockParser $parser
+    ) {
+        $this->parser = $parser;
+    }
+
+    public function scan(FileDiff $diff): DependencyCollectionPair
+    {
+        return new DependencyCollectionPair(
+            $this->parser->parse($diff->getBefore()),
+            $this->parser->parse($diff->getAfter()),
+        );
+    }
+
+    public function supports(PathInterface $path): bool
+    {
+        return $path->name() === 'pnpm-lock.yaml';
+    }
+}
