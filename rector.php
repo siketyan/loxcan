a121b22c353c4453e788b034e913a06aa1069bc9
Naoki Ikeguchi <me@s6n.jp>
2023-02-01 13:58:18 +0900

    fix: Fix rector config

--- rector.php
+++ rector.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 use Quartetcom\StaticAnalysisKit\Rector\Config;
 use Rector\CodeQuality\Rector\For_\ForRepeatedCountToOwnVariableRector;
 use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
 
 return static function (RectorConfig $rectorConfig): void {
+    Config::use($rectorConfig);
+
     $rectorConfig->paths(array_map(fn (string $path) => __DIR__ . $path, [
         '/src',
         '/tests',
     ]));
 
     $rectorConfig->skip([
+        FinalizePublicClassConstantRector::class,
         ForRepeatedCountToOwnVariableRector::class,
     ]);
-
-    Config::use($rectorConfig);
 };
